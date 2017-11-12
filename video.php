3c1e6ae527120f1b033061e6f7c5de0a71b3ed6e
root <root@Absensi-Pusdik.localdomain>
2017-11-12 22:41:09 +0700

    Video streaming sip

---
+++ video.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>The Super Ultimate Multimedia Conversion</title>
+
+    <!-- Bootstrap -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <style media="screen">
+      /* layout.css Style */
+      .upload-drop-zone {
+      height: 200px;
+      border-width: 2px;
+      margin-bottom: 20px;
+      }
+
+      /* skin.css Style*/
+      .upload-drop-zone {
+      color: #ccc;
+      border-style: dashed;
+      border-color: #ccc;
+      line-height: 200px;
+      text-align: center
+      }
+      .upload-drop-zone.drop {
+      color: #222;
+      border-color: #222;
+      }
+    </style>
+
+  </head>
+  <body style="background-color:#F8F8F8;">
+    <div class="container" style="padding-top:1em">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h4><b>The Super Ultimate Online Radio</b></h4>
+          <p style="color:#BEBEBE;">by <b>Group 5</b></p>
+        </div>
+        <div class="panel-body">
+          <video width="600" height="420" controls>
+	    <source src="http://188.166.242.112:8080/stream" type="video/ogg">
+  	    Your browser does not support the video tag.
+	  </video>
+        </div>
+        <div class="panel-footer">
+          <p style="color:#BEBEBE;">Multimedia Network Class - Department of Informatics - Institut Teknologi Sepuluh Nopember</p>
+        </div>
+      </div>
+    </div> <!-- /container -->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="bootstrap/js/jquery-3.2.1.min.js"></script>
+
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
